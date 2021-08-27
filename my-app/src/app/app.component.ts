dce12b9983b1dd506c8d96457d5a931e932448c2
Chorizard <yaninalperez@gmail.com>
2021-08-26 21:31:13 -0300

    Primer commit. Modificaciones para deploy heroku

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Usuario } from './clases/usuario';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ejercicio 1';
+  edadUno = 0;
+  edadDos = 0;
+  promedio = 0;
+  myUsuario:Usuario;
+
+  constructor() {
+    this.myUsuario = new Usuario();
+  }
+
+  calcular(){
+    this.promedio = (this.edadUno + this.edadDos)/2;
+  };
+
+  limpiar(){
+    this.promedio = (this.edadUno + this.edadDos)/2;
+  };
+}

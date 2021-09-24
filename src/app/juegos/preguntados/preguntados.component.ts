82c0710f65139f5d390140c03bfda7605500ccab
Chorizard <yaninalperez@gmail.com>
2021-09-24 10:20:41 -0300

    Load movies Preguntados game

--- src/app/juegos/preguntados/preguntados.component.ts
+++ src/app/juegos/preguntados/preguntados.component.ts
@@ -9,37 +9,33 @@ import Swal from 'sweetalert2';
 })
 export class PreguntadosComponent implements OnInit {
   random: any;
-  pickedMovie:any;
-  movieListId = [{ 'id': 694, 'name': 'El Resplandor' },
+  pickedMovie: any;
+  movieListId = [{ 'id': 694, 'name': 'The shining' }/* ,
   { 'id': 550, 'name': 'El Club de la Pelea' },
   { 'id': 493922, 'name': 'Hereditary' },
   { 'id': 530385, 'name': 'Midsommar' },
-  { 'id': 603, 'name': 'The Matrix' },
   { 'id': 348, 'name': 'Alien: Octavo Pasajero' },
   { 'id': 149, 'name': 'Akira' },
   { 'id': 128, 'name': 'La Princesa Mononoke' },
   { 'id': 62, 'name': '2001: Odisea del Espacio' },
-  { 'id': 103, 'name': 'Taxi Driver' }];
+  { 'id': 103, 'name': 'Taxi Driver' } */];
 
   imageUrl = '';
-  // El resplandor 694
-  // El club de la pelea 550
-  // Hereditary 493922
-  // 530385 Midsommar
-  // matrix 603
-  // 348 alien octavo pasajero
-  // 149 akira
-  //128 princesa mononoke
-  //62 2001 odisea del espacio
-  //103 taxi driver
+  movieName = "";
 
   constructor(private datosApi: ImagenesAPIService) {
     this.pickMovie();
+    this.loadMovie(782);
+    this.loadMovie(680);
+    this.loadMovie(493922);
+    this.loadMovie(550);
+    this.obtenerLista(782);
+    this.obtenerLista(493922);
   }
 
-  chequearPelicula(id:any) {
-    if(this.pickedMovie.id == id){
-      
+  chequearPelicula(id: any) {
+    if (this.pickedMovie.id == id) {
+
       Swal.fire({
         icon: 'success',
         title: 'Great !',
@@ -57,18 +53,58 @@ export class PreguntadosComponent implements OnInit {
       });
     }
     this.pickMovie();
-    
+
   }
 
-  pickMovie(){
+  pickMovie() {
     this.random = Math.floor((Math.random() * (this.movieListId.length - 1)));
-    this.datosApi.obtenerDatos((this.movieListId[this.random].id)).subscribe(
+    this.pickedMovie = this.movieListId[this.random];
+    this.datosApi.obtenerFrames((this.pickedMovie.id)).subscribe(
       (pelicula: any) => {
-        this.pickedMovie = this.movieListId[this.random];
         const random = Math.floor((Math.random() * (pelicula.backdrops.length - 1)));
         this.imageUrl = 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/' + pelicula.backdrops[random].file_path;
       },
       error => { console.log(error) });
+  }
+
+  loadMovie(idStart: number) {
+      this.datosApi.obtenerDatos((idStart)).subscribe(
+        (pelicula: any) => {
+          this.movieName = pelicula.title;
+          if(!this.movieListId.some(movie => movie.id === idStart))
+          {
+            if(pelicula.popularity > 14)
+            {
+              this.movieListId.push({ 'id': idStart, 'name': this.movieName });
+              this.sortList();
+            }
+          }
+        },
+        error => { console.log(error) });
+  }
+
+  obtenerLista(id: number) {
+      this.datosApi.obtenerListaGenero((id)).subscribe(
+        (peliculas: any) => {
+          peliculas.results.map((item:any) => {
+            this.loadMovie(item.id);
+          });
+        },
+        error => { console.log(error) });
+  }
+
+  compare(a: any, b: any) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  sortList(){
+    this.movieListId.sort(this.compare);
   }
 
   ngOnInit(): void {
--- src/app/servicios/imagenes-api.service.ts
+++ src/app/servicios/imagenes-api.service.ts
@@ -11,10 +11,25 @@ export class ImagenesAPIService {
   }
   // imagenes: https://www.themoviedb.org/t/p/w533_and_h300_bestv2/
 //f01de0fa0017ce1106e1fab2dd2685b7 apikey
-  public obtenerDatos(movieId:any){
+  public obtenerFrames(movieId:any){
     const url = 'https://api.themoviedb.org/3/movie/' + movieId + '/images?api_key=f01de0fa0017ce1106e1fab2dd2685b7';
     return this.http.get(url/* , 
     {headers: new HttpHeaders({'X-Api-Key': 'norcYee9ziaM/Wnd38PvAg==aZknDrLkaDdUvsot'})
   } */);
+}
+
+public obtenerDatos(movieId:any){
+  const url = 'https://api.themoviedb.org/3/movie/' + movieId + '?api_key=f01de0fa0017ce1106e1fab2dd2685b7';
+  return this.http.get(url/* , 
+  {headers: new HttpHeaders({'X-Api-Key': 'norcYee9ziaM/Wnd38PvAg==aZknDrLkaDdUvsot'})
+} */);
+}
+
+public obtenerListaGenero(movieId:number){
+  const url = 'https://api.themoviedb.org/3/movie/' + movieId + '/recommendations?api_key=f01de0fa0017ce1106e1fab2dd2685b7';
+  
+  return this.http.get(url/* , 
+  {headers: new HttpHeaders({'X-Api-Key': 'norcYee9ziaM/Wnd38PvAg==aZknDrLkaDdUvsot'})
+} */);
 }
 }
